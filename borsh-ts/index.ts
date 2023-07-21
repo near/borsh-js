e2a83100a8213a49d5ccee4eeb6a95a0d89bebc8
gagdiez <gagdiez.c@gmail.com>
2023-07-21 23:45:57 +0200

    added base encode/decode

--- borsh-ts/index.ts
+++ borsh-ts/index.ts
@@ -1,6 +1,7 @@
 import { Schema, DecodeTypes } from './types';
 import { BorshSerializer } from './serialize';
 import { BorshDeserializer } from './deserialize';
+import bs58 from 'bs58';
 
 export function serialize(schema: Schema, value: unknown): Uint8Array {
     const serializer = new BorshSerializer();
@@ -10,4 +11,17 @@ export function serialize(schema: Schema, value: unknown): Uint8Array {
 export function deserialize(schema: Schema, buffer: Uint8Array, classType?: ObjectConstructor): DecodeTypes {
     const deserializer = new BorshDeserializer(buffer);
     return deserializer.decode(schema, classType);
+}
+
+// Keeping this for compatibility reasons with the old borsh-js
+export function baseEncode(value: Uint8Array | string): string {
+    if (typeof value === 'string') {
+        // create a uint8array from string decoding utf-8 without using Buffer
+        value = new Uint8Array([...value].map((c) => c.charCodeAt(0)));
+    }
+    return bs58.encode(value);
+}
+
+export function baseDecode(value: string): Uint8Array {
+    return new Uint8Array(bs58.decode(value));
 }
