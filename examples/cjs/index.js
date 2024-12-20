88d08227aca9ea093a93b5f561d10ead0f04e364
Guillermo Alejandro Gallardo Diez <gagdiez@MacBook-Pro.home>
2024-12-20 17:20:48 +0100

    Homogenize example and remove reference to borsh-js
    The current examples differ on what they encode / decode, but more
    importantly, they refer to a `borsh-js` package that does not exist
    
    I have modified the dependencies to use the actual Borsh encoding, and
    homogenized the code so they execute exactly the same code

--- examples/cjs/index.js
+++ examples/cjs/index.js
@@ -1,4 +1,4 @@
-const borsh = require('borsh-js');
+const borsh = require('borsh');
 
 const encodedU16 = borsh.serialize('u16', 2);
 const decodedU16 = borsh.deserialize('u16', encodedU16);
@@ -7,3 +7,7 @@ console.log(decodedU16);
 const encodedStr = borsh.serialize('string', 'testing');
 const decodedStr = borsh.deserialize('string', encodedStr);
 console.log(decodedStr);
+
+const encodedU64 = borsh.serialize('u64', '100000000000000000');
+const decodedU64 = borsh.deserialize('u64', encodedU64);
+console.log(decodedU64);
